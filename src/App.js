cc5e7f7b691105620d72e62e56fac4972390dfb0
rneacy <rneacy@gmail.com>
2021-04-19 19:57:24 +0100

    Day 1 work.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './res/logo.portrait.white.notxt.svg';
+
+import MonacoEditor from 'react-monaco-editor';
+
+const editorOptions = {
+  fontSize: 18
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="Editor">
+          <MonacoEditor 
+            theme="vs-dark"
+            width="80vmax"
+            height="100vh"
+            language="cpp"
+            options={editorOptions}
+          />
+        </div>
+        <div className="Sidebar">
+          <img src={logo} className="Logo" />
+          <p>C++ Editor</p>
+        </div>
+      </div>
+    </>
   );
 }
 
