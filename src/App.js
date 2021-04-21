b4b07ee74234dc40bdac846fa44d40a86ebfe71e
rneacy <rneacy@gmail.com>
2021-04-21 12:51:09 +0100

    Added file load.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 import logo from './res/logo.portrait.white.notxt.svg';
 
+import { ToastContainer, toast, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { GoLinkExternal } from 'react-icons/go'
+import { ImCogs, ImCheckmark } from 'react-icons/im'
+import { BiImport, BiExport } from 'react-icons/bi'
+
 import MonacoEditor from 'react-monaco-editor';
-import React from 'react';
-import { useState, useEffect } from 'react';
 
 import { useFilePicker } from 'use-file-picker';
 
@@ -12,53 +20,106 @@ import { ButtonComponent } from '@syncfusion/ej2-react-buttons'
 import convert from './Convert'
 
 function App() {
-  const [code, setCode] = useState('#include "MicroBit.h"\n\nMicroBit uBit;\n\nint main(){\n\tuBit.init();\n\n\twhile(1)\n\t\tuBit.display.scroll("Hello world!");\n}');
+    const [code, setCode] = useState('#include "MicroBit.h"\n\nMicroBit uBit;\n\nint main(){\n\tuBit.init();\n\n\twhile(1)\n\t\tuBit.display.scroll("Hello world!");\n}');
+    const [editor, setEditor] = useState();
+    const [monaco, setMonaco] = useState();
 
-  const [filesContent, errors, openFileSelector, loading] = useFilePicker({
-    multiple: false,
-    readAs: 'Text', // default: "Text", availible formats: "Text" | "BinaryString" | "ArrayBuffer" | "DataURL"
-    accept: ['.cpp'],
-  });
-
-  const editorOptions = {
-    fontSize: 18
-  }
-
-  return (
-    <>
-      <div className="App">
-        <div className="Editor">
-          <MonacoEditor 
-            theme="vs-dark"
-            width="80vmax"
-            height="100vh"
-            language="cpp"
-            options={editorOptions}
-            value={code}
-          />
+    //* Import handling
+    const [filesContent, errors, openFileSelector, loading] = useFilePicker({
+        multiple: false,
+        readAs: 'Text', // default: "Text", availible formats: "Text" | "BinaryString" | "ArrayBuffer" | "DataURL"
+        accept: ['.cpp'],
+    });
+    const newFileToastMsg = ({ closeToast, toastProps }) => (
+        <div>
+            <ImCheckmark/> Loaded new file {filesContent[0]["name"]}
         </div>
-        <div className="Sidebar">
-          <div className="Header">
-            <img src={logo} className="Logo" alt="micro:bit logo" />
-            <p>C++ Editor</p>
-          </div>
+    )
+    useEffect(() => {
+        if (filesContent.length != 0) {
+            setCode(filesContent[0]["content"]);
 
-          <div className="Interaction">
-            <ButtonComponent cssClass='e-info' onClick={() => convert(code)}>
-              Compile
-            </ButtonComponent>
-            <ButtonComponent cssClass='e-info' onClick={openFileSelector}>
-              Import
-            </ButtonComponent>
-            <ButtonComponent cssClass='e-info'>
-              Save
-            </ButtonComponent>
-          </div>
-          
-        </div>
-      </div>
-    </>
-  );
+            toast.success(newFileToastMsg);
+        }
+    }, [filesContent]);
+
+    const editorOptions = {
+        fontSize: 18
+    }
+
+    const editorMount = (editor, monaco) => {
+        setEditor(editor);
+        setMonaco(monaco);
+
+        console.log(monaco);
+    }
+
+    //* Editor resize
+    useEffect(() => {
+        const resizeEditor = () => {
+            console.log("whaey");
+            editor.layout();
+        }
+
+        window.addEventListener("resize", () => resizeEditor);
+        return () => window.removeEventListener("resize", resizeEditor);
+    }, [editor]);
+
+
+    return (
+        <>
+            <div className="App">
+                <div className="Editor">
+                    <MonacoEditor 
+                        theme="vs-dark"
+                        width="80vmax"
+                        height="100vh"
+                        language="cpp"
+                        options={editorOptions}
+                        value={code}
+                        editorDidMount={editorMount}
+                    />
+                </div>
+                <div className="Sidebar">
+                    <div className="Header">
+                        <img src={logo} className="Logo" alt="micro:bit logo" />
+                        <p>C++ Editor</p>
+                    </div>
+
+                    <div className="Interaction">
+                        <ButtonComponent cssClass='e-sidebar' onClick={() => convert(code)}>
+                            <ImCogs/> Compile
+                        </ButtonComponent>
+                        <ButtonComponent cssClass='e-sidebar' onClick={openFileSelector}>
+                            <BiExport/> Import
+                        </ButtonComponent>
+                        <ButtonComponent cssClass='e-sidebar'>
+                            <BiImport/> Save
+                        </ButtonComponent>
+                    </div>
+
+                    <div className="Footer">
+                        <a href="https://rneacy.dev" className="Me" target="_blank" rel="noreferrer">rneacy<GoLinkExternal /></a>
+                    </div>
+                    
+                </div>
+            </div>
+            <div>
+                <ToastContainer
+                    position="bottom-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    transition={Zoom}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
